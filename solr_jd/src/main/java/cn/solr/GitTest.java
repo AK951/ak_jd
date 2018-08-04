aa581881e611075d8a518fd0f8f9c82abfac5152
AK <564676545@qq.com>
2018-08-04 19:49:19 +0800

    idea修改测试2

--- solr_jd/src/main/java/cn/solr/GitTest.java
+++ solr_jd/src/main/java/cn/solr/GitTest.java
@@ -20,5 +20,8 @@ package cn.solr;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git_test");
+        System.out.println("git_test2");
+
+        System.out.println("git_test22");
     }
 }
