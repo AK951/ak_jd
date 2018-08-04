7d9f723f81c91915e0bf2ed1e1ac24bb46eb88f9
AK <564676545@qq.com>
2018-08-04 19:44:19 +0800

    idea修改测试

---
+++ solr_jd/src/main/java/cn/solr/GitTest.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2018
+ * FileName: GitTest
+ * Author:   AK
+ * Date:     2018/8/4 19:42
+ * Description:
+ * History:
+ */
+
+package cn.solr;
+
+/**
+ * 〈Description〉 
+ * 〈〉
+ *
+ * @author AK
+ * @create 2018/8/4
+ * @since 1.0.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git_test");
+    }
+}
